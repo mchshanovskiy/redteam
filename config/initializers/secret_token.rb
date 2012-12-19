8bb8811a388f15aa94fa0378fe25bd3c4831b1d1
Maxim Chshanovskiy <m.chshanovskiy@outlook.com>
2012-12-19 15:38:14 +0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redteam::Application.config.secret_token = 'f612f27e580f8f3b204fe663d96b9ad9e4cd573f1a1727e6a0d2d3e17ae574f0c30fbc37e3f117cbb0ba2a9d03686ded284aeb254a9e9687d5dd9fb65713d08b'
